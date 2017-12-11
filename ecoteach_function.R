a561127ae4592db4c7abea47b9851f6da6030fef
LisaHuelsmann <lisa.huelsmann@wsl.ch>
2017-12-11 13:43:49 +0100

    simu poisson

--- ecoteach_function.R
+++ ecoteach_function.R
@@ -1,1 +1,52 @@
 # Simulating data for teaching
+
+
+
+# No overdispersion -------------------------------------------------------
+
+
+
+testData = createData(sampleSize = 200, overdispersion = 0, family = poisson())
+
+fittedModel <- glmer(observedResponse ~ Environment1 + (1|group) , family = "poisson", data = testData)
+
+sim = simulateResiduals(fittedModel)
+plot(sim)
+
+# Parametric test
+testOverdispersionParametric(fittedModel)
+
+# Omnibus test
+testOverdispersionParametric(fittedModel)
+
+# Non-parametric test
+
+sim = simulateResiduals(fittedModel,refit = T)
+plot(sim)
+testOverdispersion(sim,  plot = T)
+
+
+
+
+# Overdispersion ----------------------------------------------------------
+
+
+
+testData = createData(sampleSize = 200, overdispersion = 1, family = poisson())
+
+fittedModel <- glmer(observedResponse ~ Environment1 + (1|group) , family = "poisson", data = testData)
+
+sim = simulateResiduals(fittedModel)
+plot(sim)
+
+# Parametric test
+testOverdispersionParametric(fittedModel)
+
+# Omnibus test
+testOverdispersionParametric(fittedModel)
+
+# Non-parametric test
+
+sim = simulateResiduals(fittedModel,refit = T)
+plot(sim)
+testOverdispersion(sim,  plot = T)
